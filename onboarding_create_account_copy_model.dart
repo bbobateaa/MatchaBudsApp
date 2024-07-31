a0780b6a714601a54a2be3e513d71353db9672e8
Helen <150011687+bbobateaa@users.noreply.github.com>
2024-07-30 17:27:10 -0700

    Create onboarding_create_account_copy_model.dart

---
+++ onboarding_create_account_copy_model.dart
@@ -0,0 +1,47 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/backend/firebase_storage/storage.dart';
+import '/components/custom_appbar_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/upload_data.dart';
+import 'onboarding_create_account_copy_widget.dart'
+    show OnboardingCreateAccountCopyWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class OnboardingCreateAccountCopyModel
+    extends FlutterFlowModel<OnboardingCreateAccountCopyWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+  // Model for customAppbar component.
+  late CustomAppbarModel customAppbarModel;
+  bool isDataUploading = false;
+  FFUploadedFile uploadedLocalFile =
+      FFUploadedFile(bytes: Uint8List.fromList([]));
+  String uploadedFileUrl = '';
+
+  // State field(s) for bio widget.
+  FocusNode? bioFocusNode;
+  TextEditingController? bioTextController;
+  String? Function(BuildContext, String?)? bioTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {
+    customAppbarModel = createModel(context, () => CustomAppbarModel());
+  }
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+    customAppbarModel.dispose();
+    bioFocusNode?.dispose();
+    bioTextController?.dispose();
+  }
+}
